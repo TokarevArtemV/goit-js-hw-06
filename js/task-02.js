0e645e305399229e2177f3250c5dd5efd8a18b03
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-10-05 19:35:08 +0300

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const root = document.querySelector("#ingredients");
+
+function renderLiItems() {
+  const murkUp = [];
+  ingredients.forEach(element => {
+    const liItem = document.createElement("li");
+    liItem.textContent = element;
+    liItem.className = "item";
+    murkUp.push(liItem);
+  });
+  root.append(...murkUp)
+}
+
+renderLiItems();
+
