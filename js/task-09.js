7278744d962e1b3c9cf6138bb37cea993ebb1f82
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-10-07 00:22:13 +0300

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const root = document.body;
+const btn = document.querySelector("button[class=change-color]");
+const spanColor = document.querySelector("span[class=color]");
+
+btn.addEventListener("click", changeBackground);
+
+function changeBackground() {
+  const newColor = getRandomHexColor();
+  root.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+}
